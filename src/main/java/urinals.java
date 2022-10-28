3bea5dca08b0f72a63b1a83a294899f70113cf74
Vamsi-Somepalli <vamcsomepalli@gmail.com>
2022-10-27 17:27:38 -0700

    skeleton

---
+++ src/main/java/urinals.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+//    @author-Vamsi Krishna Somepalli____vsomepal@asu.edu
+//    @Version 1
+public class urinals {
+    public static void main(String[] args) {
+        getString();
+        openFile();
+        countUrinals();
+
+    }
+
+    public static void getString() {
+        System.out.println ("Not yet implemented");
+    }
+    public static void openFile(){
+        System.out.println ("Not yet implemented");
+    }
+    public static void countUrinals(){
+        System.out.println ("Not yet implemented");
+    }
+}
---
+++ src/test/java/urinalsTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getString() {
+    }
+
+    @Test
+    void openFile() {
+    }
+
+    @Test
+    void countUrinals() {
+    }
+}
